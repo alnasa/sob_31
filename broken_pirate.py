600667dbda5cf358e1b186d46d77ff81e54a73f7
Aleena_Rose <aleenarose3112@gmail.com>
2023-11-28 09:07:35 +0300

    debugged broken_pirate

---
+++ broken_pirate.py
@@ -0,0 +1,5 @@
+greeting = input("Hello, possible pirate! What's the password?")#closing the quotation
+if greeting in ("Arrr!"): # closing the parentheses
+	print("Go away, pirate.")
+else: #changed elif to else
+	print("Greetings, hater of pirates!") # corrected the indentation error
